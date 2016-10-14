03fc9a1300d3fe2b30276408157c7f21d9b64603
Björn Büttner <Idrinth@users.noreply.github.com>
2016-10-15 01:36:29 +0200

    Update dotd-mutik-chatmover.user.js

--- dotd-mutik-chatmover.user.js
+++ dotd-mutik-chatmover.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @name         DotD Mutik Chatmover
+// @namespace    http://idrinth.de/
+// @version      1.0.0
+// @description  moves the alliance chat provided by mutik's script to a seperate window
+// @author       Idrinth
 // @match        http://www.kongregate.com/games/5thPlanetGames/dawn-of-the-dragons
 // @grant        none
 // ==/UserScript==
@@ -13,21 +13,31 @@ window.setTimeout(function() {
 if(document.getElementById('alliance_room')) {
     window.clearInterval(idrinth4mutik);
 	document.getElementsByTagName('body')[0].appendChild(document.getElementById('alliance_room'));
-    document.getElementById('alliance_tab').addEventListener('click',function(){
+    var r=document.createElement('div');
+    r.id="idrinth_alliance_chat_wrapper";
+    r.appendChild(document.createElement('a'));
+    r.lastChild.appendChild(document.createTextNode('Alliance'));
+    r.lastChild.setAttribute('href','#');
+    r.addEventListener('click',function(){
         var el = document.getElementById('alliance_room');
-        var css = el.getAttribute('class');
+        var el2 =document.getElementById('idrinth_alliance_chat_wrapper');
+        var css = el2.getAttribute('class');
         if(css===null) {
             css='';
         }
-        if(css.match(/(^|\s)disabled(\s|$)/)) {
-            css=css.replace(/(^|\s)disabled(\s|$)/,' ');
+        if(css.match(/(^|\s)active(\s|$)/)) {
+            el.setAttribute('class','disabled');
+            el2.setAttribute('class','chat_room_tab');
         } else {
-            css=css+' disabled';
+            el2.setAttribute('class','chat_room_tab active');
+            el.setAttribute('class','');
         }
-        el.setAttribute('class',css.replace(/^\s+|\s+$/,''));
     });
+    r.setAttribute('class','chat_room_tab active');
+    document.getElementById('chat_room_tabs').appendChild(r);
     var styles = document.createElement('style');
-    styles.appendChild(document.createTextNode('#alliance_room{width:300px;background:#aaa;height:80%;position:fixed;top:10%}'
+    styles.appendChild(document.createTextNode('#alliance_tab{display:none}'
+                                               +'#alliance_room{width:300px;background:#aaa;height:80%;position:fixed;top:10%}'
                                                +'#alliance_room.disabled{display:none}'
                                                +'#alliance_room > div{padding:1px;box-sizing:border-box}'
                                                +'#alliance_users{height:20%;overflow-y:scroll;background:rgba(0,0,0,0.25);color:#ddd}'
@@ -37,11 +47,15 @@ if(document.getElementById('alliance_room')) {
                                                +'#alliance_chat_window{height:70% !important;overflow-y:scroll;overflow-x:hidden;color:#ddd}'
                                                +'#alliance_chat_window img{max-width:95%}'
                                                +'#alliance_chat_window .username{font-weight:bold;color:red}'
+                                               +'#alliance_chat_window .whisper{font-style:italic;color:#fff}'
                                                +'#alliance_chat_window .emote{color:orange}'
-                                               +'#alliance_chat_window > div > p{background:rgba(0,0,0,0.5);}'
+                                               +'#alliance_chat_window .emote .separator{display:none}'
+                                               +'#alliance_chat_window .emote .username{display:none}'
+                                               +'#alliance_chat_window a{color:lightcoral}'
+                                               +'#alliance_chat_window > div > p{background:rgba(0,0,0,0.5);overflow:hidden}'
                                                +'#alliance_chat_window > div > p.even{background:rgba(0,0,0,0.65);}'
-                                               +'#chat_controls{height:10%}'
-                                               +'#chat_controls > textarea{height:100%;with:100%;display:block}'));
+                                               +'#alliance_room > .chat_controls{height:10%}'
+                                               +'#alliance_room > .chat_controls > textarea{background:rgba(0,0,0,0.65);color:#fff;height:100%;width:100%;box-sizing:border-box;display:block}'));
 	document.getElementsByTagName('head')[0].appendChild(styles);
 }
     },1000);},1000);
